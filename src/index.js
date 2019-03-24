46c932a0d6c6c8fa58664f022a9acb5bb6758fc4
Ange <angelina.derman@mail.ru>
2019-03-24 21:35:20 +0300

    feat:Just do it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let multipliers={};  
+  let i=2;                        
+  while(i<base ){
+     while(base%i==0 && base!=i){
+      base=base/i;
+       if(!multipliers[i]){multipliers[i]=1;}
+       else{multipliers[i]++;}
+     }
+  i++;
+  }
+ if(multipliers[base]){multipliers[base]++;}
+ if(!multipliers[base]){multipliers[base]=1;}
+
+
+  let numZeros=0;
+  let minNumZeros=Number.MAX_VALUE;
+      for(let key in multipliers){
+      base=key;
+      let rememberBase=base;
+
+      while(base<number){
+      numZeros+=Math.floor(number/base);
+      base=base*rememberBase;
+      }
+
+      numZeros=Math.floor(numZeros/multipliers[key]);
+
+      if(numZeros<minNumZeros){minNumZeros=numZeros;}
+      numZeros=0;
+  }
+
+return minNumZeros;
 }
